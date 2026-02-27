289d36b23e5f50592c1d78dcb2240937315f6baf
Okkes <okkesdonbaloglu25@gmail.com>
2026-02-27 11:51:07 +0300

    trigger deploy

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ logger = logging.getLogger(__name__)
 limiter = Limiter(key_func=get_remote_address)
 
 #create api
-api = FastAPI()
+api = FastAPI() # deploy trigger
 
 # add limiter
 api.state.limiter = limiter
